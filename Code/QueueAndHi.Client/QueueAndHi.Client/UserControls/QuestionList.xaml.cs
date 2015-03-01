85455d68e1a83cec102af6572ba6c1ea32159cca
Nadav-N <nadav.naor@gmail.com>
2015-03-01 23:12:11 +0200

    Add QuestionList control

--- Code/QueueAndHi.Client/QueueAndHi.Client/UserControls/QuestionList.xaml.cs
+++ Code/QueueAndHi.Client/QueueAndHi.Client/UserControls/QuestionList.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace QueueAndHi.Client.UserControls
+namespace QueueAndHi.Client
 {
     /// <summary>
     /// Interaction logic for QuestionList.xaml
