cf6d43f97c10014611fed7607a35f8ff58cb7ae5
Nadav-N <nadav.naor@gmail.com>
2015-03-13 19:17:48 +0200

    UserAccount

---
+++ Code/QueueAndHi.Client/QueueAndHi.Client/UserAccount.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueAndHi.Client
+{
+    public class UserAccount : INotifyPropertyChanged
+    {
+        private string username;
+        private int points;
+        private bool isAdmin;
+        public bool isMuted;
+
+        public string Username
+        {
+            get
+            {
+                return this.username;
+            }
+            set
+            {
+                this.username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+
+        public int Points
+        {
+            get
+            {
+                return this.points;
+            }
+            set
+            {
+                this.points = value;
+                OnPropertyChanged("Points");
+            }
+        }
+
+        public bool IsAdmin
+        {
+            get
+            {
+                return this.isAdmin;
+            }
+            set
+            {
+                this.isAdmin = value;
+                OnPropertyChanged("isAdmin");
+            }
+        }
+
+        public bool IsMuted
+        {
+            get
+            {
+                return this.isMuted;
+            }
+            set
+            {
+                this.isMuted = value;
+                OnPropertyChanged("IsMuted");
+            }
+        }
+
+
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
