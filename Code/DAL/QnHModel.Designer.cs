10c7910eba761a71ed3a52856026ba3ccb012b6d
shoval <shoval.home@gmail.com>
2015-04-18 00:41:24 +0300

    Added the entity framework

---
+++ Code/DAL/QnHModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\tomer\Documents\GitHub\Queue-and-hi\Code\DAL\QnHModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
