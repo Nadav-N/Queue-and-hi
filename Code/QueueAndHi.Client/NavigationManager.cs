846a558603cca30b10e17742bc3dc39d07efb92d
Nadav-N <nadav.naor@gmail.com>
2015-05-16 11:29:40 +0300

    Add Navigation in PL

---
+++ Code/QueueAndHi.Client/NavigationManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueAndHi.Client
+{
+    public class NavigationManager
+    {
+        event EventHandler<NavigationRequestedEventArgs> NavigationRequested;
+    }
+
+    public class NavigationRequestedEventArgs : EventArgs
+    {
+        public Type TypeToNavigate { get; set; }
+    }
+}
