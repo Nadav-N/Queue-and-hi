0661a3defa5df9d104b4ab96d5a40a58517ec92e
shoval <shoval.home@gmail.com>
2015-05-25 00:47:29 +0300

    Added Exceptions to DAL. Added design documents

---
+++ Code/DAL/Exceptions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Exceptions
+    {
+        /// <summary>
+        /// Thrown when the DB can't be reached through the DAL
+        /// </summary>
+        public class DALConnectionError : Exception
+        {
+
+            public DALConnectionError() { }
+            public DALConnectionError(String message) : base(message) { }
+            public DALConnectionError(String message, Exception inner) : base(message, inner) { }
+        }
+
+        public class DALDuplicateKeyError : Exception
+        {
+            public DALDuplicateKeyError() { }
+            public DALDuplicateKeyError(String message) : base(message) { }
+            public DALDuplicateKeyError(string message, Exception inner) : base(message, inner) { }
+        }
+
+        public class DALZeroRowsAffectedError : Exception
+        {
+            public DALZeroRowsAffectedError () { }
+            public DALZeroRowsAffectedError (String message) : base(message) { }
+            public DALZeroRowsAffectedError(string message, Exception inner) : base(message, inner) { }
+        }
+
+        public class DALConstraintError : Exception
+        {
+            public DALConstraintError() { }
+            public DALConstraintError(String message) : base(message) { }
+            public DALConstraintError(string message, Exception inner) : base(message, inner) { }
+        }
+    }
+}
