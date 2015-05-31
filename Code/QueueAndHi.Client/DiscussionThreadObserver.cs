1d383028045342599d6a4a2bba643e311402317d
shoval <shoval.home@gmail.com>
2015-06-01 00:18:53 +0300

    Finished with the Diagrams document. Added all diagram png files Added StarUML files, but they are one big mess. you've been warned

--- Code/QueueAndHi.Client/DiscussionThreadObserver.cs
+++ Code/QueueAndHi.Client/DiscussionThreadObserver.cs
@@ -8,24 +8,34 @@ using System.Threading.Tasks;
 
 namespace QueueAndHi.Client
 {
+    //yeah, yeah, yeah, this is the observable, not the observer...
     public class DiscussionThreadObserver : IDisposable
     {
+        //subscribing to the event is the attach method.
+        //unsubscribing from it is the detach method.
         public event EventHandler<EventArgs> NewDiscussionThreadVersion;
 
-        public DiscussionThread LatestDiscussionThread { get; set; }
+        //this is not needed, it should be passed in the NewDiscussionThreadEventArgs
+        //public DiscussionThread LatestDiscussionThread { get; set; }
 
         public DiscussionThreadObserver(IPostQueries postQueries)
         {
 
         }
 
-        public DiscussionThread StartObservingDiscussionThread(int questionId)
+        //this method starts the thread that monitors the question
+        public void StartObservingDiscussionThread(int questionId)
         {
-            return null;
+            //will start the thread that polls the DAL. 
+            //inside the thread - 
+                //if there's a new version - raise the event. 
+                //the updated discussion thread will be in the eventargs.
         }
+
 
         public void Dispose()
         {
+            //dispose will kill the polling thread
             throw new NotImplementedException();
         }
     }
