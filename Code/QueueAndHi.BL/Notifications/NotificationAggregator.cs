7b194e893522dbe7b9bb75a14949c7215e2a21f9
Nadav-N <nadav.naor@gmail.com>
2015-07-14 09:00:34 +0300

    Notification Aggregator implementation
    Notification Aggregator implementation

--- Code/QueueAndHi.BL/Notifications/NotificationAggregator.cs
+++ Code/QueueAndHi.BL/Notifications/NotificationAggregator.cs
@@ -1,21 +1,72 @@
-﻿using QueueAndHi.Common;
-using QueueAndHi.Common.Logic.Validations.Question;
-using QueueAndHi.Common.Logic.Validators;
+﻿using System.ComponentModel;
 using QueueAndHi.Common.Notifications;
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Web;
+using System.Collections.Generic;
 
 namespace QueueAndHi.BL.Notifications
 {
     public class NotificationAggregator : INotificationAggregator
     {
         public IEnumerable<Notification> AggregateNotifications(IEnumerable<Notification> notifications)
-        {            
-            return null;
+        {
+            List<Notification> aggregatedNotifications = new List<Notification>();
+            foreach (NotificationType notificationType in Enum.GetValues(typeof(NotificationType)))
+            {
+                IEnumerable<Notification> filteredNotifications = notifications.Where(n => n.Type == notificationType);
+                if (filteredNotifications.Count() == 1)
+                {
+                    aggregatedNotifications.Add(filteredNotifications.Single());
+                }
+                else if (filteredNotifications.Count() > 1)
+                {
+                    aggregatedNotifications.Add(MergeSameTypeNotifications(notificationType, filteredNotifications));
+                }
+            }
+
+            return aggregatedNotifications;
+        }
+
+        private Notification MergeSameTypeNotifications(NotificationType notificationType, IEnumerable<Notification> filteredNotifications)
+        {
+            string aggregatedNotificationMessage = GetAggregatedNotificationFormat(notificationType, filteredNotifications.Count());
+            return new Notification
+            {
+                Message = aggregatedNotificationMessage,
+                Recipient = filteredNotifications.First().Recipient,
+                Seen = false,
+                // which timestamp should we put here?
+                Type = notificationType
+            };
+        }
+
+        private string GetAggregatedNotificationFormat(NotificationType notificationType, int notificationCount)
+        {
+            switch (notificationType)
+            {
+                case NotificationType.AnswerMarkedAsRight:
+                    return string.Format("There were {0} answers you posted marked as the right answer.", notificationCount);
+                case NotificationType.AnswerRankedDown:
+                    return string.Format("Different answers you posted were ranked down {0} times.", notificationCount);
+                case NotificationType.AnswerRankedUp:
+                    return string.Format("Different answers you posted were ranked up {0} times.", notificationCount);
+                case NotificationType.MarkedAsLecturer:
+                    return "You were marked as a Lecturer";
+                case NotificationType.NewAnswer:
+                    return string.Format("{0} answers were posted to your questions", notificationCount);
+                case NotificationType.PostWasDeleted:
+                    return string.Format("{0} of your posts were deleted", notificationCount);
+                case NotificationType.QuestionMarkedAsRecommended:
+                    return string.Format("{0} questions you posted were marked as recommended", notificationCount);
+                case NotificationType.QuestionRankedDown:
+                    return string.Format("Different questions you posted were ranked down {0} times.", notificationCount);
+                case NotificationType.QuestionRankedUp:
+                    return string.Format("Different questions you posted were ranked up {0} times.", notificationCount);
+                case NotificationType.YouWereMuted:
+                    return "You were muted by an administrator";
+                default:
+                    throw new InvalidEnumArgumentException("notificationType", (int)notificationType, typeof(NotificationType));
+            }
         }
     }
 }
