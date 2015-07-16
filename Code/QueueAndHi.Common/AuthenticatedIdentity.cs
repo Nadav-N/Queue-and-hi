f2d8350934660c00ce6bdd26ee4cea7271575667
Nadav-N <nadav.naor@gmail.com>
2015-07-16 09:04:09 +0300

    add AuthenticatedIdentity
    add AuthenticatedIdentity

---
+++ Code/QueueAndHi.Common/AuthenticatedIdentity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueAndHi.Common
+{
+    public class AuthenticatedIdentity
+    {
+        public int UserID { get; set; }
+        public AuthenticationToken Token { get; set; }
+    }
+}
