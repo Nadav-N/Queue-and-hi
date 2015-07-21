c189c071d77b2320e112c86ab49f468dbb5ffbe0
Nadav-N <nadav.naor@gmail.com>
2015-07-21 18:59:01 +0300

    Ranking History model add
    Ranking History model add

---
+++ Code/QueueAndHi.Common/RankingHistory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueAndHi.Common
+{
+    public class RankingHistory : List<RankingHistoryEntry>
+    {
+        public int Ranking
+        {
+            get
+            {
+                return Count(entry => entry.RankingType == RankingType.Up) - Count(entry => entry.RankingType == RankingType.Down);
+            }
+        }
+    }
+
+    public class RankingHistoryEntry
+    {
+        public RankingHistoryEntry (int userId, RankingType rankingType)
+	{
+            UserID = userId;
+            RankingType = rankingType;
+	}
+        public int UserID { get; private set; }
+        public RankingType RankingType {get; private set;}
+    }
+
+    public enum RankingType
+    {
+        Up,
+        Down
+    }
+}
