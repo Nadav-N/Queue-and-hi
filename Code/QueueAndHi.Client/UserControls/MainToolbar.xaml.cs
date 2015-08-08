4e1c8d47e3090cc643abbd3001b0bbfb079c2b14
Nadav-N <nadav.naor@gmail.com>
2015-08-08 16:26:39 +0300

    MainToolbarViewModel
    MainToolbarViewModel

--- Code/QueueAndHi.Client/UserControls/MainToolbar.xaml.cs
+++ Code/QueueAndHi.Client/UserControls/MainToolbar.xaml.cs
@@ -26,15 +26,5 @@ namespace QueueAndHi.Client
         {
             InitializeComponent();
         }
-
-        public string Text
-        {
-            get { return (string)GetValue(TextProperty); }
-            set { SetValue(TextProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty TextProperty =
-            DependencyProperty.Register("Text", typeof(string), typeof(MainToolbar), new PropertyMetadata(String.Empty));
     }
 }
